6bc807432ff90fc05798e2322f57d062e2e29327
WuhuKaichong <71682474+WuhuKaichong@users.noreply.github.com>
2023-04-06 14:36:30 +0800

    Delete CAIS/django-server-master directory

--- CAIS/django-server-master/game/urls/index.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from game.views.index import index
-
-urlpatterns = [
-    path('', index),
-    path('api/',include('game.urls.api.index')),
-]
--- CAIS/django-server-master/game/views/index.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render,redirect
-
-def index(request):
-    return render(request=request,template_name="index.html")
-
-def redirect_home(request):
-    return redirect('/')
